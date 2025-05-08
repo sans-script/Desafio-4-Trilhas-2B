bb5ccd92dfa6fb6932cca924555cb22245209d49
Pytwer <mariaraimundacarvalho1970@gmail.com>
2025-05-08 08:46:04 -0300

    api

---
+++ api/internet.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import https from 'https';
+
+const url =
+    'https://api.qedu.org.br/v1/censo/territorio?ibge_id=21&ano=2019&dependencia_id=2&ciclo_id=EM';
+
+const options = {
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer bBM1iYsPk2sbq9iUgR1iVsHzfaq6oIVKxHNnqhhE', // não apagar
+    },
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false, // apenas para testes locais
+    }),
+};
+
+axios
+    .get(url, options)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        if (error.response) {
+            console.error('Erro na requisição:', error.response.data);
+        } else if (error.request) {
+            console.error('Nenhuma resposta recebida:', error.request);
+        } else {
+            console.error('Erro ao configurar a requisição:', error.message);
+        }
+    });
